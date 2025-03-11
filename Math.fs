6a91e6352ea3949d7ab6ff82a364c8b009136aaa
cyy0303 <cyy060819@gmail.com>
2025-03-11 10:00:35 +0900

    sub

--- Math.fs
+++ Math.fs
@@ -5,5 +5,12 @@ open System
 /// Modify the function `collatz` in such a way that the function takes in a
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
-let collatz n =
-  0
+let collatz (n: int32) =
+    if n <= 0 then invalidArg "n" "Input must be a positive integer"
+    else
+        let mutable count = 0
+        let mutable num = int64 n
+        while num <> 1L do
+            num <- if num &&& 1L = 0L then num >>> 1 else 3L * num + 1L
+            count <- count + 1
+        count
